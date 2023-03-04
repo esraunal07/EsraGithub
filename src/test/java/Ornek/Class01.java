df531dd863b8e9fa12a4d5602c7c3cab317e2b2c
Esra Unal <gesraunal@gmail.com>
2023-03-04 21:14:20 +0100

    first commit

---
+++ src/test/java/Ornek/Class01.java
@@ -0,0 +1,18 @@
+package Ornek;
+
+public class Class01 {
+
+    public static void main(String[] args) {
+        System.out.println("github calismasi yapildi");
+
+
+    }
+}
+
+
+        git init
+        git add README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/esraunal07/EsraGithub.git
+        git push -u origin main
